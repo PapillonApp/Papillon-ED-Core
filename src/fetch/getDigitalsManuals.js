467bd1180e3513f2a277475df272c026f03be4b2
LeMaitre4523 <79224590+LeMaitre4523@users.noreply.github.com>
2023-12-07 19:17:23 +0100

    Fix URL getDigitalsManuals

--- src/fetch/getDigitalsManuals.js
+++ src/fetch/getDigitalsManuals.js
@@ -8,7 +8,7 @@ class getDigitalsManuals {
 
     fetch() {
         if(!this.session.findModule("MANUELS_SCOLAIRES").enable) throw errors.MODULE_DISABLE.drop("MANUELS_SCOLAIRES");
-        let url = `/E/${this.session.student.id}/manuelsNumeriques.awp?verbe=get`
+        let url = `/Eleves/${this.session.student.id}/manuelsNumeriques.awp?verbe=get`
         let body = `data={}`
         return this.session.request.post(url, body)
     }
