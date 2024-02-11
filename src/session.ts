5681658fd42bad67d659982663c8853e098f8146
camarm-dev <contact@camarm.dev>
2024-02-11 23:01:11 +0100

    [Add] implemented getgrades typed

--- src/session.ts
+++ src/session.ts
@@ -1,7 +1,7 @@
 import {Module} from "./types/modules";
+import {GetGrades} from "./fetch/getGrades";
 
 const getHomeworks = require("./fetch/getHomeworks");
-const getGrades = require("./fetch/getGrades");
 const getTimetable = require("./fetch/getTimetable");
 const getSchoollife = require("./fetch/getSchoollife");
 const getCantine = require("./fetch/getCantine");
@@ -40,12 +40,11 @@ class Session {
         this._token = null; // Le token
         this.isLoggedIn = false;
         this.settings = {}; // Les paramètres de l'utilisateur
-        this.student = {}; // Info de l'étudiant
         this.school = {}; // Info de l'etab
         this.modules = []; // Les modules
 
         this.homeworks = new getHomeworks(this);
-        this.grades = new getGrades(this);
+        this.grades = new GetGrades(this);
         this.timetable = new getTimetable(this);
         this.schoollife = new getSchoollife(this);
         this.cantine = new getCantine(this);
