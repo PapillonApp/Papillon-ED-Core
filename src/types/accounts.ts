13c31a70316264bee67638688f2b68af3e513d02
camarm-dev <contact@camarm.dev>
2024-02-11 20:42:07 +0100

    [Fix] Typing des Comptes et des requêtes auth (non testé; POC !!)

---
+++ src/types/accounts.ts
@@ -0,0 +1,68 @@
+// Types grâce à https://github.com/EduWireApps/ecoledirecte-api-docs/?tab=readme-ov-file#accounts-objects
+
+interface Account {
+    idLogin: number
+    id: number
+    uid: string
+    identifiant: string
+    typeCompte: AccountType
+    codeOgec: string
+    main: boolean
+    lastConnexion: string
+    civilite: string
+    prenom: string
+    particule: string
+    nom: string
+    email: string
+    anneeScolaireCourante: string
+    nomEtablissement: string
+    logoEtablissement: string
+    couleurAgendaEtablissement: string
+    dicoEnLigneLeRobert: boolean
+    socketToken: string
+    // TODO; type modules
+    modules: any[]
+    parametresIndividuels
+}
+
+interface AccountProfile {
+    sexe: Genre,
+    infoEDT: string,
+    nomEtablissement: string,
+    idEtablissement: string,
+    rneEtablissement: string,
+    telPortable: string,
+    idReelEtab: string,
+    photo: string,
+    classe: Class
+}
+
+interface Class {
+    id: string
+    code: string
+    libelle: string
+    estNote: number
+}
+
+interface AccountIndividualParameters {
+    lsuPoilDansLaMainBorne1: string | number, // Paramètre parcoursup
+    lsuPoilDansLaMainBorne2: string | number, // Paramètre parcoursup
+    lsuPoilDansLaMainBorne3: string | number, // Paramètre parcoursup
+    modeCalculLSU: any // Inconnu
+    isQrcode: boolean
+    accessibilitéVisuelle: boolean // Pour les personnes malvoyantes
+    typeSaisieNotesDefaut: any // Inconnu
+    nbJoursMaxRenduDevoirCDT: any // Inconnu
+    typeViewCDTDefaut: any // Inconnu
+}
+
+// TODO; connaitre tous les types de comptes
+type AccountType  = "E" | "P"
+
+
+// TODO; connaitre tous les champs sexe possibles
+type Genre  = "M" | "F"
+
+export type {
+    Account
+}
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,9 +1,25 @@
+import {Account} from "./accounts";
+
 interface AuthRequestBody {
     identifiant: string
     motdepasse: string
     isRelogin: boolean
     uuid: string
 }
+
+interface AuthRequestResponse {
+    code: AuthRequestResponseCodes
+    token: string
+    message: string
+    data: AuthRequestResponseData
+}
+
+// TODO add account types
+interface AuthRequestResponseData {
+    accounts: Account[]
+}
+
+type AuthRequestResponseCodes = 200 | 505
 
 export type {
     AuthRequestBody
