746d3c4252fe7ef8625f001253147f0e25a7585f
camarm-dev <contact@camarm.dev>
2024-02-18 11:43:17 +0100

    [Add] Code property in getCantine.ts

--- src/fetch/getCantine.ts
+++ src/fetch/getCantine.ts
@@ -15,18 +15,16 @@ class GetCantine {
         const module = this.session.findModule("CANTINE_BARCODE") as modStudBarcode;
         if(module.enable) {
             return module.params.numeroBadge;
-        } else {
-            return null;
         }
+        return undefined;
     }
 
     getReservations() {
         const module = this.session.findModule("RESERVATIONS") as modStudReservations;
         if(module.enable) {
             return module.params;
-        } else {
-            return null;
         }
+        return undefined;
     }
 
 }
