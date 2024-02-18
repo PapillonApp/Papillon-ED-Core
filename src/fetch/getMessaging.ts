bf474164e6e6fe9167ccd177089444567da5339c
camarm-dev <contact@camarm.dev>
2024-02-18 12:30:50 +0100

    [Fix] Implemented new types for messaging

--- src/fetch/getMessaging.ts
+++ src/fetch/getMessaging.ts
@@ -1,7 +1,7 @@
 import {Session} from "~/session";
 import bodyToString from "~/utils/body";
 import {mailboxRes, mailboxResData} from "~/types/v3";
-import {messagingRequestData} from "~/types/v3/requests/student";
+import {mailboxRequestData} from "~/types/v3/requests/student/mailbox";
 
 class GetMessaging {
 
@@ -16,60 +16,40 @@ class GetMessaging {
         const url = `/eleves/${this.session.student.id}/messages.awp?force=false&typeRecuperation=received&idClasseur=0&orderBy=date&order=desc&query=&onlyRead=&page=0&itemsPerPage=100&getAll=0&verbe=get`;
         const data = {
             "anneeMessages": anneeMessages
-        } as messagingRequestData;
+        } as mailboxRequestData;
         return this.session.request.post(url, bodyToString(data)).then((response: mailboxRes) => {
-            if (response.code == 200) {
-                const responseData = response.data as mailboxResData;
-                return {
-                    "classeurs": responseData.classeurs,
-                    "messages": responseData.messages.received,
-                    "parametrage": responseData.parametrage,
-                    "pagination": responseData.pagination
-                };
-            }
-        });
+            return response.data;
+        }) as Promise<mailboxResData>;
     }
 
     fetchSentMessages(anneeMessages: string = "2023-2024") {
         const url = `/eleves/${this.session.student.id}/messages.awp?force=false&typeRecuperation=sent&idClasseur=0&orderBy=date&order=desc&query=&onlyRead=&page=0&itemsPerPage=100&getAll=0&verbe=get`;
         const data = {
             "anneeMessages": anneeMessages
-        } as messagingRequestData;
+        } as mailboxRequestData;
         return this.session.request.post(url, bodyToString(data)).then((response: mailboxRes) => {
-            if (response.code == 200) {
-                const responseData = response.data as mailboxResData;
-                return {
-                    "classeurs": responseData.classeurs,
-                    "messages": responseData.messages.received,
-                    "parametrage": responseData.parametrage,
-                    "pagination": responseData.pagination
-                };
-            }
-        });
+            return response.data;
+        }) as Promise<mailboxResData>;
     }
 
     fetchMessageContentReceived(message_id: number, anneeMessages = "2023-2024") {
         const url = `/eleves/${this.session.student.id}/messages/${message_id}.awp?verbe=get&mode=destinataire`;
         const data = {
             "anneeMessages": anneeMessages
-        } as messagingRequestData;
-        return this.session.request.post(url, bodyToString(data)).then(r => {
-            return {
-                ...r.data
-            };
-        });
+        } as mailboxRequestData;
+        return this.session.request.post(url, bodyToString(data)).then((response: mailboxRes) => {
+            return response.data;
+        }) as Promise<mailboxResData>;
     }
 
     fetchMessageContentSent(message_id: number, anneeMessages = "2023-2024") {
         const url = `/eleves/${this.session.student.id}/messages/${message_id}.awp?verbe=get&mode=expediteur`;
         const data = {
             "anneeMessages": anneeMessages
-        } as messagingRequestData;
-        return this.session.request.post(url, bodyToString(data)).then(r => {
-            return {
-                ...r.data
-            };
-        });
+        } as mailboxRequestData;
+        return this.session.request.post(url, bodyToString(data)).then((response: mailboxRes) => {
+            return response.data;
+        }) as Promise<mailboxResData>;
     }
 }
 
