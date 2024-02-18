fb2a1d3b813dea053a7f15ba7559170b60f104ff
camarm-dev <contact@camarm.dev>
2024-02-18 19:12:05 +0100

    [Fix] Mistake in typing

--- src/fetch/getTimeline.ts
+++ src/fetch/getTimeline.ts
@@ -1,6 +1,6 @@
 import {Session} from "~/session";
 import bodyToString from "~/utils/body";
-import {studTlElem, studCommonTlResData} from "~/types/v3";
+import {studTlElem, studCommonTlResData, studTlRes, studCommonTlRes} from "~/types/v3";
 import {timelineRequestData} from "~/types/v3/requests";
 
 class GetTimeline {
@@ -14,13 +14,13 @@ class GetTimeline {
     async fetch(): Promise<Array<studTlElem>> {
         const url = `/E/${this.session.student.id}/timeline.awp?verbe=get`;
         const data = {} as timelineRequestData;
-        return await this.session.request.post(url, bodyToString(data)).then(response => response.data as Array<studTlElem>);
+        return await this.session.request.post(url, bodyToString(data)).then((response: studTlRes) => response.data as Array<studTlElem>);
     }
 
     async fetchCommonTimeline(): Promise<studCommonTlResData> {
         const url = `/E/${this.session.student.id}/timelineAccueilCommun.awp?verbe=get`;
         const data = {} as timelineRequestData;
-        return await this.session.request.post(url, bodyToString(data)).then(response => response.data as studCommonTlResData);
+        return await this.session.request.post(url, bodyToString(data)).then((response: studCommonTlRes) => response.data as studCommonTlResData);
     }
 }
 
