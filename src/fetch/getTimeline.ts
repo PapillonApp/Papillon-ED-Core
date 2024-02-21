f2d142c058cf3526c97fd5610a483968f4a943a3
LeMaitre4523 <79224590+LeMaitre4523@users.noreply.github.com>
2024-02-21 20:10:29 +0100

    fix: url in getTimeline

--- src/fetch/getTimeline.ts
+++ src/fetch/getTimeline.ts
@@ -12,7 +12,7 @@ class GetTimeline {
     }
 
     async fetch(): Promise<Array<studTlElem>> {
-        const url = `/E/${this.session.student.id}/timeline.awp?verbe=get`;
+        const url = `/eleves/${this.session.student.id}/timeline.awp?verbe=get`;
         const data = {} as timelineRequestData;
         return await this.session.request.post(url, bodyToString(data)).then((response: studTlRes) => response.data as Array<studTlElem>);
     }
