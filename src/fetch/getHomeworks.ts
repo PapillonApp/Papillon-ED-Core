ac211c4a03c9f7591c77fadc8e6fad01965b08df
camarm-dev <contact@camarm.dev>
2024-02-24 22:06:46 +0100

    fix: possibilité d'avoir les devoirs avec/sans les balises html dans `getHomeworks.ts` + contenu bien décodé (base64 -> string)

--- src/fetch/getHomeworks.ts
+++ src/fetch/getHomeworks.ts
@@ -1,6 +1,11 @@
 import {Session} from "~/session";
 import bodyToString from "~/utils/body";
-import {textbookDateAssignement, textbookDateRes, textbookDateResData, textbookRes} from "~/types/v3";
+import {
+    textbookDateAssignement,
+    textbookDateRes,
+    textbookDateResData,
+    textbookRes
+} from "~/types/v3";
 import {textbookRequestData, textbookSetDoneStatusRequestData} from "~/types/v3/requests/student";
 
 class GetHomeworks {
@@ -11,38 +16,46 @@ class GetHomeworks {
         this.session = session;
     }
 
+    #transformHomeworks(homeworks: Array<textbookDateAssignement>, removeHTMLTags: boolean): Array<textbookDateAssignement> {
+        homeworks.forEach((homework: textbookDateAssignement, index: number) => {
+            const aFaire = homework.aFaire;
+            if (!aFaire) return;
+            const htmlContent = atob(aFaire.contenu);
+
+            if (removeHTMLTags) {
+                const span = document.createElement("span");
+                span.innerHTML = htmlContent;
+                aFaire.contenu = span.innerText.replace(/ +/g," ");
+            } else {
+                aFaire.contenu = htmlContent;
+            }
+
+            homeworks[index].aFaire = aFaire;
+        });
+
+        return homeworks;
+    }
+
     async fetch(): Promise<textbookRes> {
         const url = `/Eleves/${this.session.student.id}/cahierdetexte.awp?verbe=get`;
         const data = {} as textbookRequestData;
-        return await this.session.request.post(url, bodyToString(data)).then(response => response as textbookRes);
+        return await this.session.request.post(url, bodyToString(data)).then((response: textbookRes) => response as textbookRes);
     }
 
-    async getByDay(day: string): Promise<textbookDateResData> {
+    async getByDay(day: string, removeHTMLTags: boolean = true): Promise<textbookDateResData> {
         const url = `/Eleves/${this.session.student.id}/cahierdetexte/${day}.awp?verbe=get`;
         const data = {} as textbookRequestData;
         return await this.session.request.post(url, bodyToString(data)).then((response: textbookDateRes) => {
-
             if (response.code == 200) {
-                const homeworks = response.data as textbookDateResData;
-
-                homeworks.matieres.forEach((homework: textbookDateAssignement, index: number) => {
-                    const aFaire= homework.aFaire;
-                    if (!aFaire) return;
-                    const htmlContent = aFaire.contenu;
-                    const span = document.createElement("span");
-                    span.innerHTML = htmlContent;
-                    // TODO: `contenuTexte` ou `contenu`
-                    aFaire.contenu = [span.textContent || span.innerText].toString().replace(/ +/g," ");
-                    homeworks.matieres[index].aFaire = aFaire;
-                });
-
-                return homeworks;
-            } else {
-                return {
-                    date: "",
-                    matieres: []
-                } as textbookDateResData;
+                const data = response.data as textbookDateResData;
+                const homeworks = data.matieres;
+                data.matieres = this.#transformHomeworks(homeworks, removeHTMLTags);
+                return data;
             }
+            return {
+                date: "",
+                matieres: []
+            } as textbookDateResData;
         });
     }
 
