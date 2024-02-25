5d22d98d123ebb8f1e07e9305571c340296ddd95
camarm-dev <contact@camarm.dev>
2024-02-25 16:54:22 +0100

    fix: fin du programme si problème d'authentification + message de connexion

--- examples/login.ts
+++ examples/login.ts
@@ -1,12 +1,12 @@
 import {EDCore} from "../index";
-import {account} from "~/types";
+import {studentAccount} from "~/types";
 
 export const ED = new EDCore();
 
 export async function login() {
     await ED.auth.login("jean", "jean%").then(() => {
-        const account = ED.student as account
-        console.log(`Logged in as ${account.identifiant}`)
+        const account = ED.student as studentAccount
+        console.log(`Logged in as ${account.civilite} ${account.prenom} ${account.nom}`)
     }).catch(err => {
         console.error(`Failed to login: Error ${err.code}: ${err.message}`);
         process.exit();
