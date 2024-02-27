da9f51090fb9a1b176ceaed24cac3d62178f0708
LeMaitre4523 <79224590+LeMaitre4523@users.noreply.github.com>
2024-02-27 15:39:54 +0100

    feat: add getDiary, getTopics, getMembers of workspace

--- src/fetch/getWorkspaces.ts
+++ src/fetch/getWorkspaces.ts
@@ -3,6 +3,10 @@ import bodyToString from "~/utils/body";
 import {joinWorkspaceRequestData, workspaceRequestData} from "~/types/v3/requests";
 import {workspacesRes, workspaceRes, workspace, emptyRes} from "~/types/v3";
 
+import { DiaryRequestData, DiaryReponseSuccess, DiaryResponseData } from "~/utils/types/workspace";
+import { TopicsResponseSuccess, TopicsResponseData } from "~/utils/types/workspace";
+import { MembersResponseSuccess, MembersResponseData } from "~/utils/types/workspace";
+
 class GetWorkspaces {
 
     session: Session;
@@ -22,6 +26,29 @@ class GetWorkspaces {
         const data = {} as workspaceRequestData;
         return await this.session.request.post(url, bodyToString(data)).then((response: workspaceRes) => response.data as workspace);
     }
+
+    async getDiary(espaceId: number): Promise<DiaryResponseData> {
+        const url = `/W/${espaceId}/agendaEvenements.awp?verbe=get`;
+        const data = {
+            "nbProchainsEvents": 0
+        } as DiaryRequestData;
+        return await this.session.request.post(url, bodyToString(data)).then((response: DiaryReponseSuccess) => response.data as DiaryResponseData);
+    }
+
+
+    async getTopics(espaceId: number): Promise<TopicsResponseData> {
+        const url = `/E/${this.session.student.id}/espacestravail/${espaceId}/topics.awp?verbe=get`;
+        const data = {};
+        return await this.session.request.post(url, bodyToString(data)).then((response: TopicsResponseSuccess) => response.data as TopicsResponseData);
+    }
+
+
+    async getMembers(espaceId: number): Promise<MembersResponseData> {
+        const url = `/E/${this.session.student.id}/espacestravail/${espaceId}/membres.awp?verbe=get`;
+        const data = {};
+        return await this.session.request.post(url, bodyToString(data)).then((response: MembersResponseSuccess) => response.data as MembersResponseData);
+    }
+
 
     async join(espace: workspace): Promise<emptyRes> {
         const url = `/E/${this.session.student.id}/espacestravail/${espace.id}/acces.awp?verbe=post`;
