abd376dcdbeb56b8be3b52dfdcf8e2dcb18d710e
camarm-dev <contact@camarm.dev>
2024-02-27 17:58:56 +0100

    fix: utilisation particule du profil

--- examples/login.ts
+++ examples/login.ts
@@ -6,7 +6,7 @@ export const ED = new EDCore();
 export async function login() {
     await ED.auth.login("jean", "jean%").then(() => {
         const account = ED.student as studentAccount;
-        console.log(`Logged in as ${account.civilite} ${account.prenom} ${account.nom}`);
+        console.log(`Logged in as ${account.particule} ${account.prenom} ${account.nom}`);
     }).catch(err => {
         console.error(`Failed to login: Error ${err.code}: ${err.message}`);
         process.exit();
