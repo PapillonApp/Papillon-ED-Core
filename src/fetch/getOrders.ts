85399ed3f7f6b790261babb10182d59b9fe6fe04
camarm-dev <contact@camarm.dev>
2024-03-09 13:31:07 +0100

    fix: updated `types` & typed orders

--- src/fetch/getOrders.ts
+++ src/fetch/getOrders.ts
@@ -1,47 +1,50 @@
 import { Session } from "~/session";
 import bodyToString from "~/utils/body";
+import {
+    ordersResData,
+    ordersResSuccess,
+    schoolMenuResData,
+    schoolMenuResSuccess,
+    startOrderResData,
+    startOrderResSuccess
+} from "~/types";
+import {body} from "~/types/v3/requests/default/body";
+
 
 class GetOrders {
-    
+
     session: Session;
 
     constructor(session: Session) {
         this.session = session;
     }
 
-    isEnabled() {
+    isEnabled(): boolean {
         return this.session.findModule("COMMANDE_PASSAGE").enable || false;
     }
 
-    async fetchSchoolMenu() {
+    async fetchSchoolMenu(): Promise<schoolMenuResData | undefined> {
         if(!this.isEnabled()) return undefined;
-        let data = {};
-        let url = "/menusRestaurationScolaire.awp?verbe=get"
-        return await this.session.request.post(url, bodyToString(data)).then((response) => {
-            return response.data;
-        })
+        const data = {} as body;
+        const url = "/menusRestaurationScolaire.awp?verbe=get";
+        return await this.session.request.post(url, bodyToString(data)).then((response: schoolMenuResSuccess) => response.data);
     }
 
-    async fetchOrders() {
+    async fetchOrders(): Promise<ordersResData | undefined> {
         if(!this.isEnabled()) return undefined;
-        let data = {};
-        let url = `/E/${this.session.student.id}/commandesPassage.awp?verbe=get`
-        return await this.session.request.post(url, bodyToString(data)).then((response) => {
-            return response.data;
-        })
+        const data = {} as body;
+        const url = `/E/${this.session.student.id}/commandesPassage.awp?verbe=get`;
+        return await this.session.request.post(url, bodyToString(data)).then((response: ordersResSuccess) => response.data);
     }
 
-    async startCommand(passageId: number, date: string) {
-        let splittedDate = date.split("-");
-        let formattedDate = `${splittedDate[0]}${splittedDate[1]}${splittedDate[2]}`;
-        let data = {};
-        let url = `/E/${this.session.student.id}/commandesPassage/pointsDePassage/${passageId}/${formattedDate}.awp?verbe=get`
-        return await this.session.request.post(url, bodyToString(data)).then((response) => {
-            return response.data;
-        })
+    async startCommand(passageId: number, date: string): Promise<startOrderResData> {
+        const splitDate = date.split("-");
+        const formattedDate = `${splitDate[0]}${splitDate[1]}${splitDate[2]}`;
+        const data = {} as body;
+        const url = `/E/${this.session.student.id}/commandesPassage/pointsDePassage/${passageId}/${formattedDate}.awp?verbe=get`;
+        return await this.session.request.post(url, bodyToString(data)).then((response: startOrderResSuccess) => response.data);
     }
 }
-
 
 export {
     GetOrders
