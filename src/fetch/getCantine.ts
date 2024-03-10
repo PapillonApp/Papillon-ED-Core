6a1ccde4e3da021791ddddaa4ddf1869fede95af
camarm-dev <contact@camarm.dev>
2024-03-10 12:45:17 +0100

    add: get school menus

--- src/fetch/getCantine.ts
+++ src/fetch/getCantine.ts
@@ -1,5 +1,25 @@
 import {Session} from "~/session";
-import {modStudBarcode, modStudReservations} from "~/types";
+import {modStudBarcode, modStudReservations, schoolMenu, schoolMenuResData, schoolMenuResSuccess} from "~/types";
+import {body} from "~/types/v3/requests/default/body";
+import bodyToString from "~/utils/body";
+
+
+class Menu {
+
+    semaine: number;
+    libelle: string;
+    documentId: number;
+
+    constructor(document: schoolMenu) {
+        this.semaine = document.semaine;
+        this.libelle = document.doc.libelle;
+        this.documentId = document.doc.id;
+    }
+
+    async getBlob() {
+      // TODO get document blob
+    }
+}
 
 
 class GetCantine {
@@ -8,7 +28,18 @@ class GetCantine {
 
     constructor(session: Session) {
         this.session = session;
+    }
 
+    async fetchSchoolMenu(): Promise<Array<Menu>> {
+        const data = {} as body;
+        const url = "/menusRestaurationScolaire.awp?verbe=get";
+        return await this.session.request.post(url, bodyToString(data)).then((response: schoolMenuResSuccess) => {
+            const menuList: Array<Menu> = [];
+            for (const menu of response.data) {
+                menuList.push(new Menu(menu))
+            }
+            return menuList
+        });
     }
 
     getBarcode(): string | undefined {
