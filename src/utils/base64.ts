f9e0980e08ec2a8b9435b0ea27e66c2db2472903
camarm-dev <contact@camarm.dev>
2024-03-15 19:58:43 +0100

    fix: documentation in `base64.ts`
    Signed-off-by: camarm-dev <contact@camarm.dev>

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,5 +1,7 @@
-/*
-* This function decodes the b64 input string, and decode uri escaped chars, if escape function exists
+/**
+ * This function decodes the b64 input string, and decode uri escaped chars, if escape function exists.
+ *
+ * Escape function allow to transform encoded chars into utf-8 ones.
 * */
 export function decodeString(value: string) {
     if (escape) {
