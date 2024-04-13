87a1ef62a389a89f69b6e08bd8d90ae054ec57e0
camarm-dev <contact@camarm.dev>
2024-04-13 17:53:49 +0200

    feat(example): meilleur output
    Signed-off-by: camarm-dev <contact@camarm.dev>

--- examples/homeworks.ts
+++ examples/homeworks.ts
@@ -4,12 +4,11 @@ import { login, ED } from "./login";
 
 login().then(() => {
     ED.homeworks.fetch().then(homeworks => {
-        console.log("Devoirs:");
         Object.keys(homeworks).forEach(key =>  {
-            console.log(`\tPour le ${key}:`);
+            console.log(`[${key}]`);
             const work = homeworks[key];
             work.forEach(subject => {
-                console.log(`\t\tDevoirs en ${subject.matiere} (${subject.codeMatiere}), donné le ${subject.donneLe}. ${subject.effectue ? "Effectué": "Non effectué"}, ${subject.interrogation ? "interrogation prévue": "pas d'interrogation"} et ${subject.rendreEnLigne ? "documents à rendre en ligne": "rien à rendre en ligne"}.`);
+                console.log(`\tDevoirs en ${subject.matiere} (${subject.codeMatiere}), donné le ${subject.donneLe}. ${subject.effectue ? "Effectué": "Non effectué"}, ${subject.interrogation ? "interrogation prévue": "pas d'interrogation"} et ${subject.rendreEnLigne ? "documents à rendre en ligne": "rien à rendre en ligne"}.`);
             });
         });
     });
--- examples/timetable.ts
+++ examples/timetable.ts
@@ -7,7 +7,7 @@ login().then(() => {
     const today = new Date();
     const todayDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
     ED.timetable.fetchByDay(todayDate).then(timetable => {
-        console.log("Emploi du temps d'aujourd'hui:");
+        console.log("\nEmploi du temps d'aujourd'hui:");
         Object.keys(timetable).forEach(key =>  {
             const matiere = timetable[key];
             console.log(`\t${matiere.text ? matiere.text: matiere.matiere} (${matiere.codeMatiere}) avec ${matiere.prof ? matiere.prof: "pas de prof"}, de ${matiere.start_date} à ${matiere.end_date} en salle ${matiere.salle ? matiere.salle: "pas de salle"}.`);
