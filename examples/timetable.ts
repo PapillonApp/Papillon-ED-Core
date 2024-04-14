b04290d08ad92aaeafa1ee71afe5eb1da5570b14
camarm-dev <contact@camarm.dev>
2024-04-14 15:04:21 +0200

    fix(type): la réponse est une liste, pas un objet
    Signed-off-by: camarm-dev <contact@camarm.dev>

--- examples/timetable.ts
+++ examples/timetable.ts
@@ -9,8 +9,7 @@ login().then(() => {
     const todayDate = `${today.getFullYear()}-${month}-${today.getDate()}`;
     ED.timetable.fetchByDay(todayDate).then(timetable => {
         console.log("\nEmploi du temps d'aujourd'hui:");
-        Object.keys(timetable).forEach(key =>  {
-            const matiere = timetable[key];
+        timetable.forEach(matiere =>  {
             console.log(`\t${matiere.text ? matiere.text: matiere.matiere} (${matiere.codeMatiere}) avec ${matiere.prof ? matiere.prof: "pas de prof"}, de ${matiere.start_date} à ${matiere.end_date} en salle ${matiere.salle ? matiere.salle: "pas de salle"}.`);
         });
     });
